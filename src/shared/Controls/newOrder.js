999ddac6a1e907823fe5d11a50ebeab8832b8df8
roy <roystern92@gmail.com>
2020-04-30 00:57:01 +0300

    styled the prices tiltes and information. I added payment method icons

--- src/shared/Controls/newOrder.js
+++ src/shared/Controls/newOrder.js
@@ -1,89 +1,91 @@
 export const controlsTypes = {
-    FULLNAME: "full name",
-    ADDRESS: "address",
-    STATUS: "status",
-    COMMUNICATION: "communication",
-  };
-  
-export const getCreateOrderControls = () => {
-    const controls = {
-      FullName: {
-        type: controlsTypes.FULLNAME,
-        elementType: "input",
-        elementConfig: {
-          type: "text",
-          placeholder: "שם מלא",
-        },
-        value: "",
-        validation: {
-          required: true,
-        },
-        valid: false,
-        touched: false,
-      },
+  FULLNAME: "full name",
+  ADDRESS: "address",
+  STATUS: "status",
+  COMMUNICATION: "communication",
+};
 
-      City: {
-        type: controlsTypes.ADDRESS,
-        elementType: "input",
-        elementConfig: {
-          type: "text",
-          placeholder: "עיר",
-        },
-        value: "",
-        validation: {
-          required: true,
-        },
-        valid: false,
-        touched: false,
+export const getCreateOrderControls = () => {
+  const controls = {
+    FullName: {
+      type: controlsTypes.FULLNAME,
+      elementType: "input",
+      elementConfig: {
+        type: "text",
+        placeholder: "שם מלא",
       },
-      
-      Street: {
-        type: controlsTypes.ADDRESS,
-        elementType: "input",
-        elementConfig: {
-          type: "text",
-          placeholder: "רחוב + מספר בית",
-        },
-        value: "",
-        validation: {
-          required: true,
-        },
-        valid: false,
-        touched: false,
+      value: "",
+      validation: {
+        required: true,
       },
-    
-    
-    
-      Amount: {
-        type: controlsTypes.COMMUNICATION,
-        elementType: "input",
-        elementConfig: {
-          type: "text",
-          placeholder: "כמות מארזים",
-        },
-        value: "",
-        validation: {
-          required: true,
-        },
-        valid: false,
-        touched: false,
+      valid: false,
+      touched: false,
+    },
+
+    City: {
+      type: controlsTypes.ADDRESS,
+      elementType: "input",
+      elementConfig: {
+        type: "text",
+        placeholder: "עיר",
       },
-      Communication: {
-        type: controlsTypes.COMMUNICATION,
-        elementType: "input",
-        elementConfig: {
-          type: "text",
-          placeholder: "אמצעי תקשורת",
-        },
-        value: "",
-        validation: {
-          required: true,
-        },
-        valid: false,
-        touched: false,
-      }
-    }
-      return controls;
-    
-    };
-    
+      value: "",
+      validation: {
+        required: true,
+      },
+      valid: false,
+      touched: false,
+    },
+
+    Street: {
+      type: controlsTypes.COMMUNICATION,
+      elementType: "input",
+      elementConfig: {
+        type: "text",
+        placeholder: "רחוב + מספר בית",
+      },
+      value: "",
+      validation: {
+        required: true,
+      },
+      valid: false,
+      touched: false,
+    },
+
+    Amount: {
+      type: controlsTypes.COMMUNICATION,
+      elementType: "select",
+      elementConfig: {
+        options: [
+          { value: "מארז 100 מסכות פנים", displayValue: "מארז 100 מסכות פנים" },
+          { value: "מארז 200 מסכות פנים", displayValue: "מארז 200 מסכות פנים" },
+          {
+            value: "מעוניין/ת בכמות גדולה יותר",
+            displayValue: "מעוניין/ת בכמות גדולה יותר",
+          },
+        ],
+      },
+      value: "מארז 100 מסכות פנים",
+      validation: {
+        required: true,
+      },
+      valid: true,
+      touched: false,
+    },
+    Communication: {
+      type: controlsTypes.COMMUNICATION,
+      elementType: "input",
+      elementConfig: {
+        type: "text",
+        placeholder: "אמצעי תקשורת",
+      },
+      value: "",
+      validation: {
+        required: true,
+      },
+      valid: false,
+      touched: false,
+    },
+  };
+  return controls;
+};
